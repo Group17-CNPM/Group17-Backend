44095a5a0d9501deed16ba03bec9ec63c8f8a068
DESKTOP-OET0P0K\Phuc <tranphuc8a@gmail.com>
2023-02-03 00:28:12 +0700

    Update db

--- sql/quanlynhankhau.sql
+++ sql/quanlynhankhau.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1:3306
--- Thời gian đã tạo: Th1 07, 2023 lúc 07:36 PM
--- Phiên bản máy phục vụ: 8.0.31
--- Phiên bản PHP: 7.4.3
+-- Thời gian đã tạo: Th2 02, 2023 lúc 06:26 PM
+-- Phiên bản máy phục vụ: 8.0.32
+-- Phiên bản PHP: 7.4.3-4ubuntu2.17
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -33,8 +33,8 @@ CREATE TABLE `danhsachnopphi` (
   `sohokhau` int NOT NULL,
   `sotien` int NOT NULL,
   `nguoinop` varchar(255) NOT NULL,
-  `ngaynop` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `ngaynop` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -49,8 +49,8 @@ CREATE TABLE `hokhau` (
   `duong` varchar(255) NOT NULL,
   `phuong` varchar(255) NOT NULL,
   `quan` varchar(255) NOT NULL,
-  `ngaylamhokhau` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `ngaylamhokhau` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Đang đổ dữ liệu cho bảng `hokhau`
@@ -67,12 +67,12 @@ INSERT INTO `hokhau` (`sohokhau`, `idchuho`, `sonha`, `duong`, `phuong`, `quan`,
 
 CREATE TABLE `khoanthu` (
   `id` int NOT NULL,
-  `ngaytao` date NOT NULL,
-  `thoihan` date NOT NULL,
+  `ngaytao` varchar(255) NOT NULL,
+  `thoihan` varchar(255) NOT NULL,
   `tenkhoanthu` varchar(255) NOT NULL,
   `batbuoc` tinyint(1) NOT NULL,
   `ghichu` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -84,9 +84,19 @@ CREATE TABLE `lichsu` (
   `id` int NOT NULL,
   `sohokhau` int NOT NULL,
   `idnhankhau` int NOT NULL,
-  `loaithaydoi` varchar(255) NOT NULL,
-  `thoigian` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `loaithaydoi` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '0: tạo mới hộ khẩu, 1: Thêm nhân khẩu vào hộ khẩu, 2: Xóa nhân khẩu khỏi hộ khẩu',
+  `thoigian` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `lichsu`
+--
+
+INSERT INTO `lichsu` (`id`, `sohokhau`, `idnhankhau`, `loaithaydoi`, `thoigian`) VALUES
+(29, 1, 7, '1', '2023-01-15 21:41:43'),
+(30, 1, 9, '1', '2023-1-16 16:7:1'),
+(31, 1, 15, '1', '2023-1-16 16:9:6'),
+(32, 1, 7, '2', '2023-2-2 23:49:51');
 
 -- --------------------------------------------------------
 
@@ -96,23 +106,18 @@ CREATE TABLE `lichsu` (
 
 CREATE TABLE `login` (
   `token` varchar(255) NOT NULL,
-  `deviceid` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `deviceid` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `username` varchar(255) NOT NULL,
-  `time` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `time` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Đang đổ dữ liệu cho bảng `login`
 --
 
 INSERT INTO `login` (`token`, `deviceid`, `username`, `time`) VALUES
-('18a35037377897f77f9e9645457fbdba', NULL, 'admin', '2023-01-07 10:12:25'),
-('24aeafccb23efe8bdba357e813deb984', NULL, 'admin', '2023-01-07 23:02:51'),
-('431b923d6d803dc0d954ad8f5fbdf7cc', NULL, 'admin', '2023-01-07 14:37:59'),
-('7f3dd2ebe62fc70f51c4a8044bd2d503', NULL, 'admin', '2023-01-07 16:02:36'),
-('b8feee21eef244ceec6e8e3989d96744', NULL, 'admin', '2023-01-07 17:05:22'),
-('c1b530d89ea0c68c7af44c62069ae695', NULL, 'admin', '2023-01-07 17:08:58'),
-('f7c0ca95abb0679987609c17aa72f412', NULL, 'admin', '2023-01-07 10:59:56');
+('35574194036a0b6e43f5297240ba7ab7', NULL, 'ketoan', '2023-2-2 23:40:36'),
+('db0fcf473f6d2c1458044326c928e228', NULL, 'admin', '2023-2-2 23:46:5');
 
 -- --------------------------------------------------------
 
@@ -122,30 +127,36 @@ INSERT INTO `login` (`token`, `deviceid`, `username`, `time`) VALUES
 
 CREATE TABLE `nhankhau` (
   `id` int NOT NULL,
-  `hoten` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `ngaysinh` date NOT NULL,
+  `hoten` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `ngaysinh` varchar(255) NOT NULL,
   `gioitinh` tinyint(1) NOT NULL,
-  `quequan` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `dantoc` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `tongiao` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `quequan` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `dantoc` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `tongiao` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `sohokhau` int DEFAULT NULL,
-  `quanhevoichuho` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `cccd` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `capngay` date DEFAULT NULL,
-  `noicap` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `nghenghiep` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `ngaydangkythuongtru` date DEFAULT NULL,
-  `ngaythemnhankhau` date DEFAULT NULL,
-  `ghichu` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `quanhevoichuho` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `cccd` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `capngay` varchar(255) DEFAULT NULL,
+  `noicap` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `nghenghiep` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `ngaydangkythuongtru` varchar(255) DEFAULT NULL,
+  `ngaythemnhankhau` varchar(255) DEFAULT NULL,
+  `ghichu` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Đang đổ dữ liệu cho bảng `nhankhau`
 --
 
 INSERT INTO `nhankhau` (`id`, `hoten`, `ngaysinh`, `gioitinh`, `quequan`, `dantoc`, `tongiao`, `sohokhau`, `quanhevoichuho`, `cccd`, `capngay`, `noicap`, `nghenghiep`, `ngaydangkythuongtru`, `ngaythemnhankhau`, `ghichu`) VALUES
-(1, 'Trần Văn Phúc batoong', '2001-04-22', 1, 'Hưng Yên', 'Kinh', 'Không', NULL, 'Là chủ hộ', '12345678910', '2022-12-01', 'Bách Khoa Hà Nội', 'Sinh viên', '2022-12-01', '2022-12-01', 'Okay'),
-(4, 'Trịnh Đức Tiệp', '2001-11-11', 1, 'Nam Định', 'Becgie', 'Tin lành', 1, 'Là con', '12345678912', '2022-12-20', 'Công an tỉnh Nam Định', NULL, NULL, NULL, NULL);
+(1, 'Trần Văn Phúc', '2001-04-22', 1, 'Hưng Yên', 'Kinh', 'Không', 1, 'Là chủ hộ', '12345678910', '2022-12-12', 'Bách Khoa Hà Nội', 'Sinh viên', '2022-12-12', '2022-12-12', 'Okay babeêrere'),
+(4, 'Trịnh Đức Tiệp', '2001-11-11', 1, 'Nam Định', 'Becgie', 'Tin lành', NULL, 'Là con', '12345678912', '2022-12-20', 'Công an tỉnh Nam Định', NULL, NULL, NULL, NULL),
+(5, 'Trịnh Đức Tiệp', '2001-11-11', 1, 'Nam Định', 'Becgie', 'Tin lành', NULL, 'Là con', '12345678913', '2022-12-20', 'Công an tỉnh Nam Định', NULL, NULL, NULL, NULL),
+(6, 'Trịnh Đức Tiệp', '2001-11-11', 1, 'Nam Định', 'Becgie', 'Tin lành', NULL, 'Là con', '12345678914', '2022-12-20', 'Công an tỉnh Nam Định', NULL, NULL, NULL, NULL),
+(7, 'Trịnh Đức Tiệp', '2001-11-11', 1, 'Nam Định', 'Becgie', 'Tin lành', NULL, 'Là con', '12345678915', '2022-12-20', 'Công an tỉnh Nam Định', NULL, NULL, NULL, NULL),
+(8, 'Trịnh Đức Tiệp', '2001-11-11', 1, 'Nam Định', 'Becgie', 'Tin lành', NULL, 'Là con', '12345678916', '2022-12-20', 'Công an tỉnh Nam Định', NULL, NULL, NULL, NULL),
+(9, 'Trịnh Đức Tiệp', '2001-11-11', 1, 'Nam Định', 'Becgie', 'Tin lành', 1, 'Là con', '12345678917', '2022-12-20', 'Công an tỉnh Nam Định', NULL, NULL, NULL, NULL),
+(15, 'Trịnh Đức Tiệp', '2001-11-11', 1, 'Nam Định', 'Becgie', 'Tin lành', 1, 'Là con', '12345678920', '2022-12-20', 'Công an tỉnh Nam Định', NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -156,11 +167,11 @@ INSERT INTO `nhankhau` (`id`, `hoten`, `ngaysinh`, `gioitinh`, `quequan`, `danto
 CREATE TABLE `tamtrutamvang` (
   `id` int NOT NULL,
   `idnhankhau` int NOT NULL,
-  `trangthai` varchar(255) NOT NULL,
-  `diachitamtru` varchar(255) NOT NULL,
-  `diachitamchutamvang` varchar(255) NOT NULL,
+  `trangthai` tinyint(1) NOT NULL,
+  `diachitamtrutamvang` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `thoigian` varchar(255) DEFAULT NULL,
   `noidungdenghi` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -172,15 +183,17 @@ CREATE TABLE `users` (
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `vaitro` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '1: Ban quản lý, 2: Kế toán'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `vaitro` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '1: Ban quản lý, 2: Kế toán'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Đang đổ dữ liệu cho bảng `users`
 --
 
 INSERT INTO `users` (`username`, `password`, `email`, `vaitro`) VALUES
-('admin', 'admin', 'tranphuc8a@gmail.com', '2');
+('admin', 'admin', 'tranphuc8a@gmail.com', '1'),
+('ketoan', 'admin', 'tranphuc8a@gmail.com', '1'),
+('ketoan2', 'admin', 'tranphuc8b@gmail.com', '1');
 
 --
 -- Chỉ mục cho các bảng đã đổ
@@ -262,13 +275,13 @@ ALTER TABLE `khoanthu`
 -- AUTO_INCREMENT cho bảng `lichsu`
 --
 ALTER TABLE `lichsu`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
 -- AUTO_INCREMENT cho bảng `nhankhau`
 --
 ALTER TABLE `nhankhau`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT cho bảng `tamtrutamvang`
