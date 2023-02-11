d878849c88e745d85ca091dd0548e5600ba52a5a
DESKTOP-OET0P0K\Phuc <tranphuc8a@gmail.com>
2023-02-12 01:04:56 +0700

    Add api thuphi

---
+++ src/model/thuphi.js
@@ -0,0 +1,199 @@
+var Utils = require('../utils/utils.js').Utils;
+/*
+- id
+- idkhoanthu
+- sohokhau
+- sotien
+- nguoinop
+- ngaynop
+- ghichu
+*/
+
+class Thuphi {
+    // static properties
+    static table = "danhsachnopphi";
+    static array_key = [
+        "id", "idkhoanthu", "sohokhau", "sotien", "nguoinop", "ngaynop", "ghichu"
+    ];
+    // non static methods => Object
+    constructor(thuphi) {
+        this.copy_from(thuphi);
+    }
+    copy_from(thuphi) {
+        if (thuphi == null) return;
+        for (let key of Thuphi.keys()) {
+            this[key] = thuphi[key] ?? this[key];
+        }
+    }
+    async save() {
+        let thuphi = await Thuphi.getById(this.id);
+        if (thuphi == null) {
+            // insert if not existed
+            return await Thuphi.insert(this);
+        } else {
+            // update if existed
+            return await Thuphi.update(this, { id: this.id });
+        }
+    }
+    async delete() {
+        return await Thuphi.delete(this);
+    }
+
+    // static methods => Table
+    static keys() { return Thuphi.array_key; }
+    static from_json(json) {
+        if (json == null) return null;
+        let thuphi = new Thuphi(json);
+        return thuphi;
+    }
+
+
+// CRUD:    
+    // Create
+    static async insert(thuphi) {
+        let connection = require('../index.js').connection;
+        let params, result;
+        params = Thuphi.keys()
+            .map(key => Utils.getSQLValue(thuphi[key]))
+            .join(', ');
+
+        let query = `INSERT INTO ${Thuphi.table} VALUE (${params})`;
+        try {
+            result = await connection.my_query(query);
+        } catch (e) {
+            console.log(e);
+            return null;
+        }
+
+        return result;
+    }
+    // Read
+    static async select(thuphi, keys = null) {
+        let connection = require('../index.js').connection;
+        let result, selectFields, whereParams;
+
+        if (keys == null || keys.length <= 0) {
+            selectFields = `*`
+        } else {
+            selectFields = keys.join(', ');
+        }
+
+        whereParams = Object.keys(thuphi)
+            .map(key => Utils.getEquation(key, thuphi[key]))
+            .join(' AND ');
+
+        let query = `SELECT ${selectFields} FROM ${Thuphi.table} WHERE TRUE ${whereParams != "" ? " AND " + whereParams : ""} ORDER BY ngaynop ASC;`;
+        try {
+            result = await connection.my_query(query);
+        } catch (e) {
+            console.log(e);
+            return null;
+        }
+
+        let list = [];
+        // console.log(result);
+        result.forEach(function (element) {
+            list.push(Thuphi.from_json(element));
+        });
+
+        return list;
+    }
+    static async getById(id, keys = null) {
+        let connection = require('../index.js').connection;
+        let result, selectFields = ``;
+        if (keys == null || keys.length <= 0) {
+            selectFields = `*`
+        } else {
+            selectFields = keys.join(', ');
+        }
+        let query = `SELECT ${selectFields} FROM ${Thuphi.table} WHERE id = ${id}`;
+        try {
+            result = await connection.my_query(query);
+        } catch (e) {
+            console.log(e);
+            return null;
+        }
+
+        return Thuphi.from_json(result[0]);
+    }
+    static async search(thuphi, keys = null) {
+        let connection = require('../index.js').connection;
+        let result, selectFields, whereParams;
+
+        if (keys == null || keys.length <= 0) {
+            selectFields = `*`
+        } else {
+            selectFields = keys.join(', ');
+        }
+
+        whereParams = Object.keys(thuphi)
+            .filter(key => thuphi[key])
+            .map(key => Utils.getSearchEquation(key, thuphi[key], false))
+            .join(' AND ');
+
+        // console.log(Object.keys(thuphi));
+
+        let query = `SELECT ${selectFields} FROM ${Thuphi.table} WHERE TRUE ${whereParams != "" ? " AND " + whereParams : ""};`;
+        try {
+            result = await connection.my_query(query);
+        } catch (e) {
+            console.log(e);
+            return null;
+        }
+
+        let list = [];
+        result.forEach(function (element) {
+            list.push(Thuphi.from_json(element));
+        });
+
+        return list;
+    }
+    // Update
+    static async update(thuphi, where) {
+        if (thuphi == null || where == null) {
+            throw "Không đủ tham số cho hàm update";
+        }
+        let connection = require('../index.js').connection;
+        let result;
+        let setParams, whereParams;
+
+        setParams = Object.keys(thuphi)
+            .map(key => `${key} = ${Utils.getSQLValue(thuphi[key])}`)
+            .join(', ');
+
+        whereParams = Object.keys(where).
+            map(key => Utils.getEquation(key, where[key], true))
+            .join(', ');
+
+        let query = `UPDATE ${Thuphi.table} SET ${setParams} WHERE TRUE ${whereParams != "" ? " AND " + whereParams : ""}`;
+        try {
+            result = await connection.my_query(query);
+        } catch (e) {
+            console.log(e);
+            return null;
+        }
+
+        return result;
+    }
+    // Delete
+    static async delete(thuphi) {
+        let connection = require('../index.js').connection;
+        let result;
+        let whereParams = Object.keys(thuphi)
+            .map(key => Utils.getEquation(key, thuphi[key], true))
+            .join(', ');
+
+        let query = `DELETE FROM ${Thuphi.table} WHERE TRUE ${whereParams != "" ? " AND " + whereParams : ""}`;
+        try {
+            result = await connection.my_query(query);
+        } catch (e) {
+            console.log(e);
+            return null;
+        }
+
+        return result;
+    }
+}
+
+
+module.exports = { Thuphi };
---
+++ src/router/thuphi_router.js
@@ -0,0 +1,83 @@
+
+
+var ThuphiController = require('../controller/thuphi_controller.js').ThuphiController;
+
+class ThuphiRouter{
+	constructor(app){
+		this.app = app;
+	}
+	route(){
+		
+		this.app.route("/getKhoanthu")
+			.get(function(req, res){
+				let thuphiController = new ThuphiController();
+				thuphiController.getKhoanthu(req, res);
+			});
+		this.app.route("/getDetailKhoanthu")
+			.get(function(req, res){
+				let thuphiController = new ThuphiController();
+				thuphiController.getDetailKhoanthu(req, res);
+			});
+		this.app.route("/getListKhoanthu")
+			.get(function(req, res){
+				let thuphiController = new ThuphiController();
+				thuphiController.getListKhoanthu(req, res);
+			});
+		this.app.route("/searchKhoanthu")
+			.get(function(req, res){
+				let thuphiController = new ThuphiController();
+				thuphiController.searchKhoanthu(req, res);
+			});
+		this.app.route("/addKhoanthu")
+			.get(function(req, res){
+				let thuphiController = new ThuphiController();
+				thuphiController.addKhoanthu(req, res);
+			});
+		this.app.route("/updateKhoanthu")
+			.get(function(req, res){
+				let thuphiController = new ThuphiController();
+				thuphiController.updateKhoanthu(req, res);
+			});
+		this.app.route("/deleteKhoanthu")
+			.get(function(req, res){
+				let thuphiController = new ThuphiController();
+				thuphiController.deleteKhoanthu(req, res);
+			});
+
+		this.app.route("/addThuphi")
+			.get(function(req, res){
+				let thuphiController = new ThuphiController();
+				thuphiController.addThuphi(req, res);
+			});
+		this.app.route("/updateThuphi")
+			.get(function(req, res){
+				let thuphiController = new ThuphiController();
+				thuphiController.updateThuphi(req, res);
+			});
+		this.app.route("/deleteThuphi")
+			.get(function(req, res){
+				let thuphiController = new ThuphiController();
+				thuphiController.deleteThuphi(req, res);
+			});
+		this.app.route("/getThuphi")
+			.get(function(req, res){
+				let thuphiController = new ThuphiController();
+				thuphiController.getThuphi(req, res);
+			});
+		this.app.route("/getListThuphi")
+			.get(function(req, res){
+				let thuphiController = new ThuphiController();
+				thuphiController.getListThuphi(req, res);
+			});
+		this.app.route("/searchThuphi")
+			.get(function(req, res){
+				let thuphiController = new ThuphiController();
+				thuphiController.searchThuphi(req, res);
+			});
+
+
+	}
+}
+
+module.exports = { ThuphiRouter };
+
